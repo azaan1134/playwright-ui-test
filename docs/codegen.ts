d15f0a937aaef9e4239c639fbb84bc5c34f8f332
azaan1134 <azjargala076@gmail.com>
2026-09-08 20:19:43 +0800

    Codegen-ээр үүсгэсэн код, тест амжилттай ажилсан баримт

---
+++ docs/codegen.ts
@@ -0,0 +1,51 @@
+//Codegen-ээр бичүүлсэн тестүүд
+
+import { test, expect } from '@playwright/test';
+// 1. Амжилттай нэвтрэх
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+});
+
+
+// 2. Амжилтгүй нэвтрэх
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('dbbdbd');
+  await page.locator('[data-test="login-button"]').click();
+});
+
+// 3. Бараа эрэмбэлэх
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('dbbdbd');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByText('Swag LabsEpic sadface:').click();
+  await page.locator('[data-test="password"]').dblclick();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="password"]').press('Enter');
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="product-sort-container"]').selectOption('lohi');
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+});
