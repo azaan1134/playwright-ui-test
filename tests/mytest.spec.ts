964360345a59d768724307409aff6dcbe439b380
azaan1134 <azjargala076@gmail.com>
2026-09-08 19:19:05 +0800

    Амжилтгүй нэвтрэх тест

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -23,4 +23,25 @@ test.describe('Saucedemo тест', () => {
     await expect(page).toHaveURL('https://www.saucedemo.com/');
     await expect(page.getByPlaceholder('Username')).toBeVisible();
   });
+
+    test('2. Амжилтгүй нэвтрэх', async ({ page }) => {
+    // Нэвтрэх хуудас руу шилжинэ
+    await page.goto('https://www.saucedemo.com/');
+
+    // Буруу нууц үгээр нэвтрэх оролдлого хийнэ
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('wrong_password');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // Нэвтэрч чадалгүй тухайн хуудас дээрээ үлдсэнийг шалгана
+    await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+    // Алдааны мэдээлэл таарж байгаа эсэхийг шалгана
+    await expect(
+      page.getByText(
+        'Epic sadface: Username and password do not match any user in this service',
+        { exact: true }
+      )
+    ).toBeVisible();    
+  });
 });
